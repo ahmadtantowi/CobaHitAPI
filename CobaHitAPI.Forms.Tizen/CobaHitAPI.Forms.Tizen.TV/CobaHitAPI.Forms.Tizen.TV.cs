804fc483ca5eb29ff3c10a013e9065c358fdabb6
ahmadtantowi <ahmadtantowi@outlook.com>
2019-01-10 17:43:01 +0700

    Add tizen project

---
+++ CobaHitAPI.Forms.Tizen/CobaHitAPI.Forms.Tizen.TV/CobaHitAPI.Forms.Tizen.TV.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace CobaHitAPI.Forms.Tizen
+{
+    class Program : global::Xamarin.Forms.Platform.Tizen.FormsApplication
+    {
+        protected override void OnCreate()
+        {
+            base.OnCreate();
+            // Call 'LoadApplication(Application application)' here to load your application.
+            LoadApplication(new App());
+        }
+
+        static void Main(string[] args)
+        {
+            var app = new Program();
+            global::Xamarin.Forms.Platform.Tizen.Forms.Init(app);
+            app.Run(args);
+        }
+    }
+}
