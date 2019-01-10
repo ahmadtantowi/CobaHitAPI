50979f619dd0f56fc5b62f27f4b5084ecb8ad97a
ahmadtantowi <ahmadtantowi@outlook.com>
2019-01-10 13:32:31 +0700

    Add project for hit API

--- CobaHitAPI.Forms/CobaHitAPI.Forms/MainPage.xaml.cs
+++ CobaHitAPI.Forms/CobaHitAPI.Forms/MainPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CobaHitAPI.Interfaces;
+using Refit;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +14,35 @@ namespace CobaHitAPI.Forms
         public MainPage()
         {
             InitializeComponent();
+            entryName.Completed += BtnCheck_Clicked;
+        }
+
+        private async void BtnCheck_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                aiBusy.IsRunning = true;
+                Models.User result = await RestService.For<IGitHubAPI>("https://api.github.com").GetUser(entryName.Text);
+
+                imgUser.Source = result?.avatar_url;
+                lblName.Text = result?.name ?? "nama tidak tersedia";
+
+                var gesture = new TapGestureRecognizer();
+                gesture.Tapped += (s, ev) => Device.OpenUri(new Uri(result.html_url, UriKind.Absolute));
+                var span = new Span { Text = result.html_url, TextDecorations = TextDecorations.Underline, TextColor = Color.Blue };
+                span.GestureRecognizers.Add(gesture);
+                var fstr = new FormattedString();
+                fstr.Spans.Add(span);
+                lblUrl.FormattedText = fstr;
+            }
+            catch (Exception exc)
+            {
+                await App.Current.MainPage.DisplayAlert("Error", exc.Message, "OKE");
+            }
+            finally
+            {
+                aiBusy.IsRunning = false;
+            }
         }
     }
 }
---
+++ CobaHitAPI/Interfaces/IGitHubAPI.cs
@@ -0,0 +1,16 @@
+﻿using CobaHitAPI.Models;
+using Refit;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CobaHitAPI.Interfaces
+{
+    public interface IGitHubAPI
+    {
+        [Headers("User-Agent: CobaHitAPI")]
+        [Get("/users/{user}")]
+        Task<User> GetUser(string user);
+    }
+}
